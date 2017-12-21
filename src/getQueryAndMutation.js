aa4f272e5e5782ac7fa30f8d69e7174d221a2579
Tocknicsu <wingemerald@gmail.com>
2017-12-21 10:12:27 +0800

    can cancel default generate query & mutation

--- src/getQueryAndMutation.js
+++ src/getQueryAndMutation.js
@@ -31,40 +31,43 @@ export default ({
   const descriptionResolver = utils.getQueryDescription
   let graphqlObj
   // query all
-  // TODO: resolve: utils.funcWrapper(modelConfig.crud.read.one.resolve, [resolver(model)])
-  graphqlObj = modelConfig.crud.read.all({
-    name: nameResolver(model, 'read', 'all'),
-    description: descriptionResolver(model, 'read', 'all'),
-    type: utils.createNonNullList(modelType),
-    resolve: utils.createNonNullListResolver(resolver(model, {
-      list: true
-    }))
-  })
-  result.queries[graphqlObj.name] = graphqlObj
-  // query one
-  graphqlObj = modelConfig.crud.read.one({
-    name: nameResolver(model, 'read', 'one'),
-    description: descriptionResolver(model, 'read', 'one'),
-    type: modelType,
-    args: {
-      id: {
-        type: new GraphQLNonNull(GraphQLID)
-      }
-    },
-    resolve: resolver(model, {
-      before: (findOptions, args) => {
-        if (args.id) {
-          let {type, id} = fromGlobalId(args.id)
-          findOptions.where = {
-            ...findOptions.where,
-            id
-          }
-        }
-        return findOptions
-      }
+  if (modelConfig.crud.read.all) {
+    graphqlObj = modelConfig.crud.read.all({
+      name: nameResolver(model, 'read', 'all'),
+      description: descriptionResolver(model, 'read', 'all'),
+      type: utils.createNonNullList(modelType),
+      resolve: utils.createNonNullListResolver(resolver(model, {
+        list: true
+      }))
     })
-  })
-  result.queries[graphqlObj.name] = graphqlObj
+    result.queries[graphqlObj.name] = graphqlObj
+  }
+  // query one
+  if (modelConfig.crud.read.one) {
+    graphqlObj = modelConfig.crud.read.one({
+      name: nameResolver(model, 'read', 'one'),
+      description: descriptionResolver(model, 'read', 'one'),
+      type: modelType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve: resolver(model, {
+        before: (findOptions, args) => {
+          if (args.id) {
+            let {type, id} = fromGlobalId(args.id)
+            findOptions.where = {
+              ...findOptions.where,
+              id
+            }
+          }
+          return findOptions
+        }
+      })
+    })
+    result.queries[graphqlObj.name] = graphqlObj
+  }
 
   let defaultFields = attributeFields(model, defaults(modelConfig.fieldConfig, {
     commentToDescription: true
@@ -79,89 +82,95 @@ export default ({
     fields: defaultFields
   })
 
-  graphqlObj = modelConfig.crud.create.one({
-    name: nameResolver(model, 'create', 'one'),
-    description: descriptionResolver(model, 'create', 'one'),
-    inputFields: () => {
-      return {
-        values: {
-          type: valuesFieldType
+  if (modelConfig.crud.create.one) {
+    graphqlObj = modelConfig.crud.create.one({
+      name: nameResolver(model, 'create', 'one'),
+      description: descriptionResolver(model, 'create', 'one'),
+      inputFields: () => {
+        return {
+          values: {
+            type: valuesFieldType
+          }
+        }
+      },
+      outputFields: () => {
+        return {
+          [utils.getTableName(model)]: {
+            type: modelType
+          }
+        }
+      },
+      mutateAndGetPayload: async (...args) => {
+        utils.convertFieldsFromGlobalId(defaultFields, args[0].values)
+        const instance = await model.create(args[0].values)
+        return {
+          [utils.getTableName(model)]: instance
         }
       }
-    },
-    outputFields: () => {
-      return {
-        [utils.getTableName(model)]: {
-          type: modelType
-        }
-      }
-    },
-    mutateAndGetPayload: async (...args) => {
-      utils.convertFieldsFromGlobalId(defaultFields, args[0].values)
-      const instance = await model.create(args[0].values)
-      return {
-        [utils.getTableName(model)]: instance
-      }
-    }
-  })
+    })
+    result.mutations[graphqlObj.name] = mutationWithClientMutationId(graphqlObj)
+  }
 
-  result.mutations[graphqlObj.name] = mutationWithClientMutationId(graphqlObj)
-
-  graphqlObj = modelConfig.crud.update.one({
-    name: nameResolver(model, 'update', 'one'),
-    description: descriptionResolver(model, 'update', 'one'),
-    inputFields: () => {
-      return {
-        id: {
-          type: new GraphQLNonNull(GraphQLID)
-        },
-        values: {
-          type: valuesFieldType
+  if (modelConfig.crud.update.one) {
+    graphqlObj = modelConfig.crud.update.one({
+      name: nameResolver(model, 'update', 'one'),
+      description: descriptionResolver(model, 'update', 'one'),
+      inputFields: () => {
+        return {
+          id: {
+            type: new GraphQLNonNull(GraphQLID)
+          },
+          values: {
+            type: valuesFieldType
+          }
+        }
+      },
+      outputFields: () => {
+        return {
+          [utils.getTableName(model)]: {
+            type: modelType
+          }
+        }
+      },
+      mutateAndGetPayload: async (...args) => {
+        utils.convertFieldsFromGlobalId(defaultFields, args[0].values)
+        args[0].id = fromGlobalId(args[0].id).id
+        let instance = await model.findById(args[0].id)
+        await instance.update(args[0].values)
+        return {
+          [utils.getTableName(model)]: instance
         }
       }
-    },
-    outputFields: () => {
-      return {
-        [utils.getTableName(model)]: {
-          type: modelType
+    })
+
+    result.mutations[graphqlObj.name] = mutationWithClientMutationId(graphqlObj)
+  }
+
+  if (modelConfig.crud.delete.one) {
+    graphqlObj = modelConfig.crud.delete.one({
+      name: nameResolver(model, 'delete', 'one'),
+      description: descriptionResolver(model, 'delete', 'one'),
+      inputFields: () => {
+        return {
+          id: {
+            type: new GraphQLNonNull(GraphQLID)
+          }
+        }
+      },
+      outputFields: () => {
+        return {
+        }
+      },
+      mutateAndGetPayload: async (...args) => {
+        args[0].id = fromGlobalId(args[0].id).id
+        let instance = await model.findById(args[0].id)
+        await instance.destroy()
+        return {
         }
       }
-    },
-    mutateAndGetPayload: async (...args) => {
-      utils.convertFieldsFromGlobalId(defaultFields, args[0].values)
-      args[0].id = fromGlobalId(args[0].id).id
-      let instance = await model.findById(args[0].id)
-      await instance.update(args[0].values)
-      return {
-        [utils.getTableName(model)]: instance
-      }
-    }
-  })
+    })
+    result.mutations[graphqlObj.name] = mutationWithClientMutationId(graphqlObj)
+  }
 
-  result.mutations[graphqlObj.name] = mutationWithClientMutationId(graphqlObj)
-
-  graphqlObj = modelConfig.crud.delete.one({
-    name: nameResolver(model, 'delete', 'one'),
-    description: descriptionResolver(model, 'delete', 'one'),
-    inputFields: () => {
-      return {
-        id: {
-          type: new GraphQLNonNull(GraphQLID)
-        }
-      }
-    },
-    outputFields: () => {
-      return {
-      }
-    },
-    mutateAndGetPayload: async (...args) => {
-      args[0].id = fromGlobalId(args[0].id).id
-      let instance = await model.findById(args[0].id)
-      await instance.destroy()
-      return {
-      }
-    }
-  })
-  result.mutations[graphqlObj.name] = mutationWithClientMutationId(graphqlObj)
   return result
 }
